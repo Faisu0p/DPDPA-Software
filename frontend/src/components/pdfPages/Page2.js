2c0841d40bc8e535ae668cbb9bbba749afecd6b2
Faisu <faisal.khan786hi@gmail.com>
2025-01-25 16:42:51 +0530

    styling changes in page 2

--- frontend/src/components/pdfPages/Page2.js
+++ frontend/src/components/pdfPages/Page2.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Page2.css';
 import logo from './images/dp-logo.jpg';
+import 'font-awesome/css/font-awesome.min.css';
 
 const Page2 = () => {
   return (
@@ -35,12 +36,13 @@ const Page2 = () => {
         <div className="pdf-page2-report-sections">
           <div className="pdf-page2-section-item">
             <div className="pdf-page2-section-content">
-              <h3>1. Risk Analysis</h3>
+              <h3>1. Risk Analysis and Zero-Day Vulnerabilities <i className="fa fa-shield"></i></h3>
               <p>
-              This section provides a detailed assessment of 
-              potential risks based on the preliminary data provided. 
-              It highlights key risk factors that need attention and 
-              helps identify areas of concern for further evaluation.
+              This section provides a detailed assessment of potential
+               risks based on the preliminary data provided.It highlights 
+               key risk factors, including zero-day vulnerabilities, 
+               that need attention and helps identify areas of concern 
+               for further evaluation and mitigation.
               </p>
             </div>
             <div className="pdf-page2-section-icon pdf-page2-health-icon"></div>
@@ -48,7 +50,7 @@ const Page2 = () => {
 
           <div className="pdf-page2-section-item">
             <div className="pdf-page2-section-content">
-              <h3>2. Historical Charts</h3>
+              <h3>2. Historical Charts <i className="fa fa-line-chart"></i></h3>
               <p>
               This section presents comparative charts that illustrate 
               how identified risks have evolved over time. These visual 
@@ -61,12 +63,13 @@ const Page2 = () => {
 
           <div className="pdf-page2-section-item">
             <div className="pdf-page2-section-content">
-              <h3>3. Calculated Test Results</h3>
+              <h3>3. Calculated Vulnerabilities Results <i className="fa fa-bug"></i></h3>
               <p>
-              Here, we provide a thorough analysis of the results from 
-              various tests conducted as part of the risk assessment process. 
-              These results have been calculated to identify critical risk areas, 
-              ensuring accuracy and clarity.
+              Here, we provide a thorough analysis of the results 
+              from various tests conducted to assess vulnerabilities.
+              These results have been calculated to identify critical 
+              vulnerability areas, ensuring accuracy and clarity in 
+              addressing potential risks.
               </p>
             </div>
             <div className="pdf-page2-section-icon pdf-page2-lab-icon"></div>
@@ -74,22 +77,33 @@ const Page2 = () => {
 
           <div className="pdf-page2-section-item">
             <div className="pdf-page2-section-content">
-              <h3>4. Risk Advisory</h3>
-              <p>
-              This advisory provides actionable recommendations to 
-              mitigate or manage the identified risks. It includes 
-              suggestions for adjustments to processes, further assessments, 
-              or consultations if needed to address any significant concerns.
-              </p>
+            <h3>4. Cybersecurity Threat Landscape <i className="fa fa-exclamation-triangle"></i></h3>
+            <p>
+              This section provides an overview of the current cybersecurity 
+              threat landscape, highlighting emerging threats and critical 
+              areas of concern.It includes an analysis of key threats such 
+              as data breaches and ransomware attacks, offering insights 
+              into potential risks and areas for vigilance.
+            </p>
+            <ul>
+              <li><strong>Data Breaches:</strong> Insights into the growing risks 
+              associated with unauthorized access to sensitive information, along 
+              with best practices for protection.</li>
+              
+              <li><strong>Ransomware Attacks:</strong> A look at the increasing 
+              frequency of ransomware attacks, their potential impact, and 
+              strategies to mitigate these threats.</li>
+            </ul>
+
             </div>
             <div className="pdf-page2-section-icon pdf-page2-advisory-icon"></div>
           </div>
 
           <div className="pdf-page2-section-item">
             <div className="pdf-page2-section-content">
-              <h3>5. General Recommendations</h3>
+              <h3>5. General Recommendations <i className="fa fa-thumb-tack"></i></h3>
               <p>
-              In this section, you’ll find a set of general recommendations 
+              In this section, you'll find a set of general recommendations 
               for minimizing potential risks, tailored to different scenarios. 
               The recommendations are categorized to provide clear guidance on 
               how often you should perform certain checks or updates based on 
